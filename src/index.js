ef86aa01c0086df4319eff96f448552cd5882f73
fralonra <fralonra@aliyun.com>
2020-01-19 15:44:37 +0800

    use cra built-in css reset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'normalize.css/normalize.css'
 import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
